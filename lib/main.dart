4f756372d2a23c77c34b1410b0963437ebae5f13
JuneHee0324 <alfzm2020@gmail.com>
2024-08-13 09:05:55 +0900

    3강

--- lib/main.dart
+++ lib/main.dart
@@ -13,14 +13,27 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       // Scaffold 상 중 하 로 나눈다.
       home : Scaffold(
+        appBar : AppBar(title : Text('앱임')),
+        body : Align(
+          alignment: Alignment.topRight, 
+          child : Container(
+            width: 150, height: 50, color : Colors.red,
+            // margin : EdgeInsets.all(20),
+            decoration: BoxDecoration(
+              // border : Border.all(color : Colors.black)
+
+            ),
+            // child : Text('dddd')
+          )
+        ),
       // appBar: Appbar(),
-      body : Row(
-        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-        children: [
-          Icon(Icons.star),
-          Icon(Icons.star)
-          ],
-        ),  
+      // body : Row(
+        // mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        // children: [
+          // Icon(Icons.star),
+          // Icon(Icons.star)
+          // ],
+        // ),  
       ),
     );
   }
